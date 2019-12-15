5d48a96528696df5abf7988c493d343b39255dd1
softbase <contact@softbase.co.kr>
2019-12-15 21:38:44 +0900

    2019/12/15 메모장 스토리보드 구성 및 데이터 모델 추가

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by base soft on 15/12/2019.
+//  Copyright © 2019 swift. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMemory/Model/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by base soft on 15/12/2019.
+//  Copyright © 2019 swift. All rights reserved.
+//
+
+import UIKit
+
+class MemoData {
+    var MemoIdx: Int? // 데이터 식별값
+    var title: String? // 메모 제목
+    var contents: String? // 메모 내용
+    var Image: UIImage? // 이미지
+    var regdate: Date? // 작성일
+}
